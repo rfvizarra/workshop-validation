36f64b5d5de9b608d29fd69e536564a91bb8c8a1
Anler Hernandez Peral <anler@anler.me>
2016-05-20 16:40:28 +0200

    Solución al ejercicio 2

--- ex2/Main.hs
+++ ex2/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import Data.Maybe (fromMaybe)
+import System.Environment (getArgs)
+import Text.Read (readMaybe)
+
 main :: IO ()
-main = putStrLn "solution ex2"
+main = do
+  args <- getArgs
+  let filePath = head args
+  fileContents <- readFile filePath
+  let result = sumLines fileContents
+  putStrLn $ show result
+
+
+sumLines :: String -> Integer
+sumLines = sum . map numOrZero . lines
+  where
+    numOrZero :: String -> Integer
+    numOrZero = fromMaybe 0 . readMaybe
