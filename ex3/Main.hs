c30bf9bde598a5e95cf7e92ffb93ad4e255eb242
Anler Hernandez Peral <anler@anler.me>
2016-05-20 17:14:40 +0200

    Solución al ejercicio 3

--- ex3/Main.hs
+++ ex3/Main.hs
@@ -1,4 +1,50 @@
 module Main where
 
+import Control.Category ((>>>))
+import Data.Either (partitionEithers)
+import Data.List (intersperse)
+import System.Environment (getArgs)
+import Text.Printf (printf)
+import Text.Read (readMaybe)
+
 main :: IO ()
-main = putStrLn "solution ex3"
+main = do
+  args <- getArgs
+  let filePath = head args
+  fileContents <- readFile filePath
+  let (errors, ints) = parseLines fileContents
+  if length errors > 0
+    then printErrors errors
+    else printSum ints
+
+
+printErrors :: [String] -> IO ()
+printErrors = putStrLn . concat . intersperse "\n"
+
+
+printSum :: [Integer] -> IO ()
+printSum = putStrLn . show . sum
+
+
+parseLines :: String -> ([String], [Integer])
+parseLines =
+  lines >>>
+  enumerate >>>
+  map parseLine >>>
+  partitionEithers
+
+
+enumerate :: [String] -> [(Integer, String)]
+enumerate = zip [1..]
+
+
+parseLine :: (Integer, String) -> Either String Integer
+parseLine (lineNumber, line) =
+  case readMaybe line of
+    Just integer -> Right integer
+    Nothing -> Left $ getErrorMessage lineNumber line
+
+
+getErrorMessage :: Integer -> String -> String
+getErrorMessage lineNumber line =
+  printf "Wrong number '%s' at line: %d" line lineNumber
