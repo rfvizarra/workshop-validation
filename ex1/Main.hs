1b21228e510e519a6f95c63c27c03a17be4ff158
Rodrigo Fernandez-Vizarra <rfvizarra@gmail.com>
2016-05-26 21:35:14 +0200

    Exercise 1

--- ex1/Main.hs
+++ ex1/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import System.Environment (getArgs)
+
 main :: IO ()
-main = putStrLn "solution ex1"
+main = do
+  args <- getArgs
+  let filePath = head args
+  fileContents <- readFile filePath
+  putStrLn . show . sumFileContents $ fileContents
+
+
+sumFileContents :: (Num a, Read a) => String -> a
+sumFileContents = sum . map  read . lines
