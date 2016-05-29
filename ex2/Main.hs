9356d99f365e8ffd0c6d7e2221f92bf90afd0c74
Rodrigo Fernandez-Vizarra <rfvizarra@gmail.com>
2016-05-29 20:47:26 +0200

    Exercise 2 completed

--- ex2/Main.hs
+++ ex2/Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
+
+import System.Environment (getArgs)
+import Text.Read
 main :: IO ()
-main = putStrLn "solution ex2"
+main = do
+  args <- getArgs
+  let filePath = head args
+  fileContents <- readFile filePath
+  putStrLn . show . sumFileContents $ fileContents
+
+hasData :: (Num a) => Maybe a -> Bool
+hasData n =
+  case n of
+   Just _ -> True
+   _ -> False
+
+getNum :: (Num a) => Maybe a -> a
+getNum (Just x) = x
+
+sumFileContents :: (Num a, Read a) => String -> a
+sumFileContents = sum . map getNum . filter hasData . map readMaybe . lines
